e81cbae5360badfbddebb98ac33d45abbb377a9a
Hunter1110 <wenfeng_zhang@aopen.com>
2017-08-02 13:22:20 +0800

    测试是否会重启

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Users' });
+  res.render('index', { title: 'Express Users test' });
 });
 
 module.exports = router;
